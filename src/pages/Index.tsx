918337e280e4d8a0f3fc2f72ac25d64ce715a2d6
Yevhen Molchaniuk <jekajakorev@gmail.com>
2025-03-08 14:30:00 +0200

    add time and location

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,8 +26,8 @@ const upcomingEvents = [
         id: '1',
         title: 'Міжнародний жіночий день',
         date: '8 вересня 2025',
-        // time: '18:00',
-        // location: 'Актова зала'
+        time: '18:00',
+        location: 'Актова зала'
     }
 ];
 
