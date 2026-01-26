c2a285ed047eca9e27bec99c9c10ddf92e2d72db
Yevhen Molchaniuk <jekajakorev@gmail.com>
2026-01-26 10:30:49 +0200

    Облік та вступ, додавання документів

--- src/data/legal-information.ts
+++ src/data/legal-information.ts
@@ -1,9 +1,10 @@
-import {BarChart3, FileText} from "lucide-react";
+import {BarChart3, FileText, Languages} from "lucide-react";
 
 export const categories = [
     {id: 'all', name: 'Всі'},
     // {id: 'general', name: 'Загальна інформація'},
     // {id: 'education', name: 'Освітній процес'},
+    {id: 'admission', name: 'Вступ'},
     {id: 'administrative', name: 'Адміністративні питання'}
 ];
 
@@ -55,7 +56,7 @@ export const legalSections = [
         id: 'stratehiya-rozvytku',
         categories: ['administrative'],
         title: 'Стратегія розвитку Таїровського ліцею',
-        icon: BarChart3,
+        icon: FileText,
         content: "Стратегія розвитку Таїровського ліцею на 2025 - 2030 роки. Документ визначає основні напрямки розвитку закладу, цілі та завдання на найближчі роки.",
         documents: [
             {
@@ -71,11 +72,39 @@ export const legalSections = [
         ]
     },
     {
+        id: 'oblik-ditej',
+        categories: ['administrative'],
+        title: 'Тереторія обслуговування Таїровського ліцею',
+        icon: FileText,
+        content: "Тереторія обслуговування Таїровського ліцею",
+        documents: [
+            {
+                label: 'Облік дітей (PDF)',
+                url: "/files/oblik-ditej.pdf",
+                type: 'pdf'
+            }
+        ]
+    },
+    {
         id: 'mova',
         categories: ['administrative'],
         title: 'Мова освітнього процесу',
-        icon: BarChart3,
+        icon: Languages,
         content: `Відповідно до <a href='https://zakon.rada.gov.ua/laws/show/2145-19#n111'>статті 7 Закону України «Про освіту»</a> мовою освітнього процесу в закладах освіти є державна мова.\nВідповідно до <a href="https://zakon.rada.gov.ua/laws/show/2704-19#n16">п.1 статті 1 Закону України «Про забезпечення функціонування української мови як державної»</a> єдиною державною (офіційною) мовою в Україні є українська мова.`,
         documents: []
+    },
+    {
+        id: 'prijom-documentiv-2026',
+        categories: ['admission'],
+        title: 'Прийом документів для вступу у 1-ий клас, 2026 рік',
+        icon: FileText,
+        content: `З <b>02 березня по 29 травня 2026 року</b> буде здійснюватися прийом документів для вступу дітей до <b>1-го класу</b> Таїровського ліцею Таїровської селищної ради Одеського району Одеської області`,
+        documents: [
+            {
+                label: 'Прийом документів (PDF)',
+                url: "/files/prijom-documentiv-2026.pdf",
+                type: 'pdf'
+            }
+        ]
     }
 ];
